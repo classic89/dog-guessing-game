0c98bca618c4092a34b053552623cc0b1fa8e1e5
classic89 <retrorock8821@gmail.com>
2019-03-04 04:25:43 -0600

    adding missing files

--- momtestpenny/momtestpenny/settings.py
+++ momtestpenny/momtestpenny/settings.py
@@ -77,11 +77,7 @@ WSGI_APPLICATION = 'momtestpenny.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
+        'NAME': os.path.join(BASE_DIR, 'db.sqli
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
