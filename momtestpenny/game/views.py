85ed73e8d7f1626db05d00165d96c56c597de351
classic89 <retrorock8821@gmail.com>
2019-03-04 04:46:43 -0600

    create a view for game

--- momtestpenny/game/views.py
+++ momtestpenny/game/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def my_view(request):
+    return render(request, 'game.html', {'foo':'bar'},
+    content_type='application/xhtml+xml')
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
