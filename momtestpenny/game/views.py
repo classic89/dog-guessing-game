ace9b1384250d7a13a8dc9947283cac0b0315061
classic89 <retrorock8821@gmail.com>
2019-03-04 04:51:41 -0600

    adding a url

--- momtestpenny/game/views.py
+++ momtestpenny/game/views.py
@@ -4,6 +4,3 @@ from django.shortcuts import render
 def my_view(request):
     return render(request, 'game.html', {'foo':'bar'},
     content_type='application/xhtml+xml')
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
